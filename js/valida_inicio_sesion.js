118372902a6bebdc5abf435be8f78ef1cc787ff0
matialvarez7 <matias.gabriel.alvarez@gmail.com>
2024-05-26 22:23:24 -0300

    Agregada validaciones de campos

--- js/valida_inicio_sesion.js
+++ js/valida_inicio_sesion.js
@@ -1,45 +1,80 @@
-const formulario = document.getElementById('formInicioSesion');
+document.addEventListener('DOMContentLoaded', ()=>{
+    const formulario = document.getElementById('formInicioSesion');
+    
+    formulario.addEventListener('submit', evento =>{
+        evento.preventDefault()
+    
+        if(validarFormulario()){
+            evento.preventDefault()
+            console.log("El formualario es valido")
+        }else{
+            evento.preventDefault()
+            console.log("El formulario no es valido")
+        }
+    })
 
-formulario.addEventListener('submit', evento =>{
-    evento.preventDefault()
+    function validarFormulario(){
+        let formularioValido = true;
+        formularioValido = validarCampoEmail('email', "el correo electrónico no es válido") && formularioValido;
+        formularioValido = validarCampoFormulario('password', "ingresar contraseña") && formularioValido;
+        return formularioValido;
+    }
 
-    if(validarFormulario()){
-        formulario.submit();
+
+    function validarEmail(email){
+        const mailRegEx = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        mailRegEx.test(email);
+    }
+
+    function mostrarError(campo, mensaje){
+        const divPadre = campo.parentNode;
+        const error = document.querySelector('.error-text');
+        divPadre.classList.add('error');
+        error.innerText = mensaje;
+    }
+
+    function eliminarError(input){
+        const divPadre = input.parentNode;
+        divPadre.classList.remove('error');
+        const error = document.querySelector('.error-text');
+        error.innerText = '';
+    }
+
+    formulario.querySelectorAll('input').forEach(input =>{
+        input.addEventListener('change',()=>{
+            const valor = input.value.trim();
+            if(valor !== ''){
+                eliminarError(input);
+            }
+        })
+    })
+
+    function validarCampoFormulario(id, mensaje){
+        const campo = document.getElementById(id);
+        const valor = campo.value.trim();
+        if(valor === ''){
+            mostrarError(campo, mensaje);
+            return false;
+        }else{
+            eliminarError(campo)
+            return true;
+        }
+    }
+
+    function validarCampoEmail(id, mensaje){
+        const campo = document.getElementById(id);
+        const email = campo.value.trim();
+        if(email === ''){
+            mostrarError(campo, 'Ingresar correo electrónico');
+            return false;
+        }else if(!validarEmail(email)){
+            mostrarError(campo, mensaje);
+            return false
+        }else{
+            eliminarError(campo);
+            return true
+        }
+    
     }
 })
 
-function validarFormulario(){
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    
-    if (email === ''){
-        mostarError('email', 'Por favor ingresar un email')
-        return false;
-    }else{
-        mostarError('email','')
-    }
-    if(!validarEmail){
-        mostarError('email', 'Por favor ingresar un email válido')
-        return false;
-    }else{
-        mostarError('email','')
-    }
-    if(password === ''){
-        mostarError('password', 'Ingresar una contraseña')
-        return false;
-    }else{
-        mostarError('password','')
-    }
-    return true;
-
-}
-
-function validarEmail(email){
-    const mailRegEx = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    mailRegEx.test(email);
-}
-
-function mostarError(campo, mensaje){
-    const campoError = document.getElementById(`${campo}-error`);
-    campoError.innerText = mensaje;
-}
